951b56ebc1f9306a1ffde1f765d1bd1ee58f98cd
caiyuannan <292585665@qq.com>
2019-12-15 10:16:46 +0800

    Initial commit

---
+++ src/test/java/com/bus/BusApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}

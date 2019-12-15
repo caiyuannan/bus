7a2ede4c5eb36c6c0a53a53bb4304152c4490677
angelXie <ha707164@163.com>
2019-12-16 00:32:06 +0800

    mapper接口、service接口 -- 谢海安

---
+++ src/main/java/com/bus/mapper/XhaCityConfigurationMapper.java
@@ -0,0 +1,3 @@
+package com.bus.mapper;
+
+public interface XhaCityConfigurationMapper {}
---
+++ src/main/java/com/bus/service/XhaCityConfigurationService.java
@@ -0,0 +1,9 @@
+package com.bus.service;
+
+/**
+ * 城市配置业务
+ * by 谢海安
+ */
+public interface XhaCityConfigurationService {
+
+}

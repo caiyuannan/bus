501465d6103f04870d550e2987f3e3b60583f1b5
“demaxiya2” <406511367@qq.com>
2019-12-16 08:58:37 +0800

    Merge branch 'master' of D:\bus with conflicts.

--- src/main/java/com/bus/mapper/XhaCityConfigurationMapper.java
+++ src/main/java/com/bus/mapper/XhaCityConfigurationMapper.java
@@ -1,16 +1,6 @@
-
 package com.bus.mapper;
-import com.bus.javabean.XhaCityBean;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import java.util.List;
-
 /**
  * 城市配置mapper
  * by 谢海安
  */
-@Mapper
-public interface XhaCityConfigurationMapper {
-	/**查询城市*/
-	public List<XhaCityBean> findCity(/*@Param("provinceName") String provinceName, @Param("cityName") String cityName*/);
-}
+public interface XhaCityConfigurationMapper {}

459209a1f65ebad7a1650cfffa7124e39ddbf669
angelXie <ha707164@163.com>
2019-12-16 09:03:57 +0800

    试试

--- src/main/java/com/bus/BusApplication.java
+++ src/main/java/com/bus/BusApplication.java
@@ -1,9 +1,11 @@
 package com.bus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.bus.mapper")
 public class BusApplication
 {
 
